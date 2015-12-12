e2ea7e47124d263cfdd551c26b165c320e3c44c5
Rok Kralj <info@rok-kralj.net>
2015-12-12 15:49:02 +0100

    Day 8: Shorten code a bit.

--- 8-1.scala
+++ 8-1.scala
@@ -1,7 +1,7 @@
 val regex = raw"(\\+)(x[^\\]{2}|.)".r
 
 println(io.Source.stdin.getLines.map { line =>
-	regex.findAllIn(line).matchData.map { m =>
+	regex.findAllMatchIn(line).map { m =>
 		val backslashes = m.group(1).size
 		val evenNumber = backslashes % 2 == 0
 		backslashes/2 + (if (evenNumber) 0 else m.group(2).size)
